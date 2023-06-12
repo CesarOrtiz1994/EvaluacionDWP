f735be0b609a669943807d8b2c3d0fd958380a29
Cesar Ortiz <cesarortize94@gmail.com>
2023-06-12 17:30:23 -0600

    inicio del proyecto

---
+++ src/main/java/mx/edu/uteq/evaluacionidgs06/EvaluacionIdgs06Application.java
@@ -0,0 +1,13 @@
+package mx.edu.uteq.evaluacionidgs06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionIdgs06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvaluacionIdgs06Application.class, args);
+    }
+
+}
---
+++ src/test/java/mx/edu/uteq/evaluacionidgs06/EvaluacionIdgs06ApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.uteq.evaluacionidgs06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluacionIdgs06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
